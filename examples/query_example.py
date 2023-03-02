d6c87f8ef6a94cd7865f4e77faf3d58715c38468
hdelc <hdelc02@gmail.com>
2023-03-02 17:02:22 -0500

    Add a cross-sectional query to the examples

--- examples/query_example.py
+++ examples/query_example.py
@@ -2,7 +2,7 @@ import requests
 
 z = requests.get(
     "http://localhost:5010/keyword_search",
-    json={"keyword_query": "optimus prime", "aggregate_time_period": "day"},
+    json={"keyword_query": "optimus prime", "aggregate_time_period": "day", "cross_sections": ["voterbase_race", "voterbase_gender"]},
 )
 print(z.json())
 #z = requests.post(
