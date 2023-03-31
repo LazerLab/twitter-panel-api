0518ec92bccac2093e312c0ed17b6d7093be076e
hdelc <hdelc02@gmail.com>
2023-03-31 09:47:22 -0400

    docs: Update example query to include date range

--- examples/query_example.py
+++ examples/query_example.py
@@ -6,6 +6,8 @@ z = requests.get(
         "keyword_query": "optimus prime",
         "aggregate_time_period": "day",
         "cross_sections": ["voterbase_race", "voterbase_gender"],
+        "after": "2020-10-01",
+        "before": "2020-11-01",
     },
 )
 print(z.json())
